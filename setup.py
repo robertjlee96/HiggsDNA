66d94ffba9c011475c718cc14093c6c7b022b6eb
Massimiliano Galli <massimiliano.galli@cern.ch>
2022-02-10 11:03:38 +0100

    Update setup files to the ones generated with cookie; also .flake8 not needed if stored in setup.cfg

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# Copyright (c) 2022, Hgg Group
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://gitlab.cern.ch/HiggsDNA-project/HiggsDNA for details.
+
+from __future__ import annotations
+
 from setuptools import setup
 
 setup()
+
+# This file is optional, on recent versions of pip you can remove it and even
+# still get editable installs.

058603b614552dbd820d79192b0b11b4ec1985fe
Jan Lukas Spah <janlukas.spaeh@physik.rwth-aachen.de>
2022-12-02 19:40:48 +0100

    Fixing minor bugs with ptTagger and yTagger

--- higgs_dna/workflows/taggers/__init__.py
+++ higgs_dna/workflows/taggers/__init__.py
@@ -3,7 +3,7 @@ from typing import Any, Dict
 from .DummyTagger1 import DummyTagger1
 from .DummyTagger2 import DummyTagger2
 from .HHWWggTagger import HHWWggTagger
-from .etaTagger import etaTagger
+from .yTagger import yTagger
 from .ptTagger import ptTagger
 
 taggers: Dict[str, Any] = {}

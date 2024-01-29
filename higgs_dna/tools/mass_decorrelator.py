60fb8f4d34cf8c5baeefd26527afd0b24efee6e6
Jan Lukas Spah <janlukas.spaeh@physik.rwth-aachen.de>
2024-01-29 20:13:34 +0100

    Resolve "Investigate impact of adding theory-driven photon cuts which solve linear-term sensitivity in IR region"

--- higgs_dna/tools/mass_decorrelator.py
+++ higgs_dna/tools/mass_decorrelator.py
@@ -5,17 +5,55 @@ import numpy as np
 import higgs_dna.tools.decorrelator as decorr
 
 
-def decorrelate_mass_resolution(events: awkward.Array):
+def decorrelate_mass_resolution(events: awkward.Array, type: str, year):
+
+    # type = "nominal","smeared","corr","corr_smeared"
 
     # reading the CDFs files
     var,dVar,ref = "sigma_m_over_m","mass",125.0   # Varable to be decorrelated, decorrelated w.r.t and the reference bin
     dummyDf = pandas.DataFrame({'{}'.format(var): [0], '{}'.format(dVar): [0]})
     decl = decorr.decorrelator(dummyDf, var, dVar, np.linspace(100., 180., 161))
-    decl.loadCdfs(os.path.dirname(__file__) + '/Smeared_Diphoton_CDFs.pkl.gz')
 
     # setting up the decorrelator
     df = pandas.DataFrame()
-    df["sigma_m_over_m"] = events.sigma_m_over_m_Smeared.to_numpy()  # Reading directly the smeared sigma_m_over_m
+
+    if (type == "nominal"):
+
+        if (year == "2022postEE"):
+            decl.loadCdfs(os.path.dirname(__file__) + '/decorrelation_CDFs/postEE/nominal_sigma_m_postEE_CDFs.pkl.gz')
+        else:
+            decl.loadCdfs(os.path.dirname(__file__) + '/decorrelation_CDFs/preEE/nominal_sigma_m_preEE_CDFs.pkl.gz')
+        df["sigma_m_over_m"] = events.sigma_m_over_m.to_numpy()
+
+    elif (type == "smeared"):
+
+        if (year == "2022postEE"):
+            decl.loadCdfs(os.path.dirname(__file__) + '/decorrelation_CDFs/postEE/sigma_m_smeared_postEE_CDFs.pkl.gz')
+        else:
+            decl.loadCdfs(os.path.dirname(__file__) + '/decorrelation_CDFs/preEE/sigma_m_smeared_preEE_CDFs.pkl.gz')
+        df["sigma_m_over_m"] = events.sigma_m_over_m_Smeared.to_numpy()
+
+    elif (type == "corr"):
+
+        if (year == "2022postEE"):
+            decl.loadCdfs(os.path.dirname(__file__) + '/decorrelation_CDFs/postEE/sigma_m_corr_postEE_CDFs.pkl.gz')
+        else:
+            decl.loadCdfs(os.path.dirname(__file__) + '/decorrelation_CDFs/preEE/sigma_m_corr_preEE_CDFs.pkl.gz')
+        df["sigma_m_over_m"] = events.sigma_m_over_m_corr.to_numpy()
+
+    elif (type == "corr_smeared"):
+
+        if (year == "2022postEE"):
+            decl.loadCdfs(os.path.dirname(__file__) + '/decorrelation_CDFs/postEE/sigma_m_smeared_corr_postEE_CDFs.pkl.gz')
+        else:
+            decl.loadCdfs(os.path.dirname(__file__) + '/decorrelation_CDFs/preEE/sigma_m_smeared_corr_preEE_CDFs.pkl.gz')
+        df["sigma_m_over_m"] = events.sigma_m_over_m_Smeared_corr.to_numpy()
+
+    else:
+        print("Specify a valid type: nominal,smeared,corr,corr_smeared")
+        exit()
+
+    # Reading directly the smeared sigma_m_over_m
     df["mass"] = events.mass.to_numpy()
     df["weight"] = events.weight.to_numpy()
 
--- higgs_dna/workflows/taggers/fiducialGeometricTagger.py
+++ higgs_dna/workflows/taggers/fiducialGeometricTagger.py
@@ -6,13 +6,13 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class fiducialTagger:
+class fiducialGeometricTagger:
     def __init__(self) -> None:
         pass
 
     @property
     def name(self) -> str:
-        return "fiducialTagger"
+        return "fiducialGeometricTagger"
 
     # lower priority is better
     # first decimal point is category within tag (in case for untagged)
@@ -33,7 +33,7 @@ class fiducialTagger:
         """
         selection = PackedSelection()
 
-        # This tagger determines if an event passes the fiducial selection at gen level
+        # This tagger determines if an event passes the fiducial selection at gen level using the geometric cuts proposed in 2106.08329
         # Therefore, we need to protect data events from this tagger as they do not have the relevant field
         if 'GenIsolatedPhoton' in events.fields:  # if we are processind MC
             selection.add("n_iso_photon", ak.num(events.GenIsolatedPhoton, axis=1) > 1)
@@ -41,7 +41,7 @@ class fiducialTagger:
             lead_pho = events.GenIsolatedPhoton[:, 0]
             sublead_pho = events.GenIsolatedPhoton[:, 1]
             diphoton = (lead_pho) + (sublead_pho)
-            selection.add("lead_photon_scaled_pt", lead_pho.pt / diphoton.mass > 1 / 3)
+            selection.add("sqrt_product_scaled_pt", (np.sqrt(lead_pho.pt * sublead_pho.pt) / diphoton.mass > 1 / 3))
             selection.add("sublead_photon_scaled_pt", sublead_pho.pt / diphoton.mass > 1 / 4)
             selection.add(
                 "lead_photon_eta",
@@ -56,7 +56,7 @@ class fiducialTagger:
 
             sel_fiducial = selection.all(
                 "n_iso_photon",
-                "lead_photon_scaled_pt",
+                "sqrt_product_scaled_pt",
                 "sublead_photon_scaled_pt",
                 "lead_photon_eta",
                 "sublead_photon_eta",
@@ -66,7 +66,7 @@ class fiducialTagger:
                 before: \t {len(events)}
                 ---
                 n_iso_photon: \t {ak.sum(selection.all("n_iso_photon"))}
-                lead_photon_scaled_pt: \t {ak.sum(selection.all("lead_photon_scaled_pt"))}
+                sqrt_product_scaled_pt: \t {ak.sum(selection.all("sqrt_product_scaled_pt"))}
                 sublead_photon_scaled_pt: \t {ak.sum(selection.all("sublead_photon_scaled_pt"))}
                 lead_photon_eta: \t {ak.sum(selection.all("lead_photon_eta"))}
                 sublead_photon_eta: \t {ak.sum(selection.all("sublead_photon_eta"))}
@@ -82,7 +82,7 @@ class fiducialTagger:
                 {},
             )
         else:  # we are processing data
-            logger.debug("Processing data and you want the fiducialTagger. Thus, code of -99 is applied for all events in data.")
+            logger.debug("Processing data and you want the fiducialGeometricTagger. Thus, code of -99 is applied for all events in data.")
             return (
                 ak.ones_like(events.event) * -99,  # -99 is our code for data here
                 {},
